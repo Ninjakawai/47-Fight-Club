4366ea0c39b11e7ce031f20ff44b43283703e988
Ninjakawai <nathan.vidal47@gmail.com>
2026-01-28 15:00:40 +0100

    ajout des images et correction des balises "title" pour toutes les pages, et modification du CSS Hall of Fame, HeaderFooter et Histoire, ainsi que le JS du headerFooter

--- js/headerFooterLoader.js
+++ js/headerFooterLoader.js
@@ -6,7 +6,7 @@ let header = `      <div id="main"></div>
           <li><a href="/">Accueil</a></li>
           <li><a href="/histoire">Histoire</a></li>
           <li><a href="/contact">Contact</a></li>
-          <li><a href="/nous- rejoindre" class="link-btn_red">Nous rejoindre</a></li>
+          <li><a href="/nous-rejoindre" class="link-btn_red">Nous rejoindre</a></li>
         </ul>
     </div>
     <header>
@@ -18,7 +18,7 @@ let header = `      <div id="main"></div>
         <a href="#open">
           <i class="fa-solid fa-bars header-burger_icon"></i>
         </a>
-        <ul>
+        <ul id="sommaire-header">
           <li><a href="/">Accueil</a></li>
           <li><a href="/histoire">Histoire</a></li>
           <li><a href="/contact">Contact</a></li>
